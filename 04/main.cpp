e62565909411376e2db5f8f7fbbdfc704c1cd973
Olya Const <elyaishere@mail.ru>
2020-11-04 14:52:54 +0300

    Update main.cpp

--- 04/main.cpp
+++ 04/main.cpp
@@ -5,6 +5,8 @@ int main() {
     BigInt a = 1;
     BigInt b("123456789012345678901234567890");
 
+    assert (a != b);
+
     std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
 
@@ -17,6 +19,23 @@ int main() {
 
     a = d + b;
     std::cout << "a = " << a <<std::endl;
+
+    std::cout << "a - a = " << (a - a) << std::endl;
+    std::cout << "a - b = " << (a - b) << std::endl;
+    std::cout << "b - d = " << (b - d) << std::endl;
+
+    std::cout << "500000000 + (1) = " << BigInt(500000000) + (1) << std::endl;
+    std::cout << "500000000 + (-1) = " << BigInt(500000000) + (-1) << std::endl;
+    std::cout << "-500000000 + (-1) = " << BigInt(-500000000) + (-1) << std::endl;
+    std::cout << "-500000000 + (1) = " << BigInt(-500000000) + (1) << std::endl;
+
+    std::cout << "500000000 !=  (-1) = " << (BigInt(500000000) != BigInt(-1)) << std::endl;
+    std::cout << "-1 != -1 = " << (BigInt(-1) != BigInt(-1)) << std::endl;
+
+    std::cout << "1111 * 1111 = " << BigInt(1111) * 1111 << std::endl;
+    std::cout << "1111 * (-1111) = " << BigInt(1111) * (-1111) << std::endl;
+
+    
 
     return 0;
 }
