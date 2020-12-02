2037b00b4d192793c25f24862f8476c4d5146cff
Olya Const <elyaishere@mail.ru>
2020-12-02 21:11:22 +0300

    Update main.cpp

--- 08/main.cpp
+++ 08/main.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 
 int main() {
+    ThreadPool pool(8);
     // test 1 
     {
         struct A {
@@ -11,21 +12,20 @@ int main() {
         };
         auto foo = [](const A& p) { return p.a;};
 
-        ThreadPool pool(8);
-
         auto task1 = pool.exec(foo, A());
         assert(task1.get() == 5);
 
         auto task2 = pool.exec([]() { return 1; });
         assert(task2.get() == 1);
     }
+    
     // test 2
     {
         auto boo = [](int x) { return x*x; };
-        ThreadPool pool(4);
         auto task = pool.exec(boo, 25);
         assert(task.get() == 25*25);
     }
+    
     std::cout << "All tests passed" << std::endl;
     return 0;
 }
